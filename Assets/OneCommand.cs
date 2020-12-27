293dd4c139357f2f90327a25c22dfa581dd4410e
Prince-JH <aljihoon@ajou.ac.kr>
2020-12-27 18:24:31 +0900

    update

--- Assets/OneCommand.cs
+++ Assets/OneCommand.cs
@@ -7,15 +7,20 @@ using UnityEngine.SceneManagement;
 public class OneCommand : MonoBehaviour
 {
     public GameObject Column, Floor, White, Quit, Booster;
-    public AudioSource BirdSound_1, BirdSound_2, BirdSound_3, BirdSound_4;
+    public AudioSource BirdSound_1, BirdSound_2, BirdSound_3, BirdSound_4, BoostSound;
     public Text Score;
     Rigidbody2D rig;
-    float NextTime = 1.7f;
-    float boostTime = 0f;
+    float NextTime = 2.0f;
+    float boostTime = 7.0f;
     int i, j, k, BestScore = 0;
     bool Stop = false;
+    bool isBoost = false;
+    [Header("파티클 시스템(부스터)")]
+    [SerializeField] ParticleSystem boostEffect;
     GameObject[] gameObjects = new GameObject[3];
     GameObject[] boosters = new GameObject[3];
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -31,8 +36,8 @@ public class OneCommand : MonoBehaviour
             Application.Quit();
         GetComponent<Animator>().SetFloat("Velocity", rig.velocity.y);
 
-        if (gameObject.transform.position.y > 4.75f)
-            transform.position = new Vector3(-1.5f, 4.75f, 0);
+        if (gameObject.transform.position.y > 4.65f)
+            transform.position = new Vector3(-1.5f, 4.65f, 0);
         if (gameObject.transform.position.y < -2.55f)
         {
             rig.simulated = false;
@@ -52,62 +57,24 @@ public class OneCommand : MonoBehaviour
             rig.AddForce(Vector3.up * 270);
             BirdSound_1.Play();
         }
-        //기둥 생성기
-        if(Time.time > NextTime)
-        {
-            NextTime = Time.time + 1.7f;
-            gameObjects[j] = (GameObject)Instantiate(Column, new Vector3(4, Random.Range(-1f, 3.2f), 0), Quaternion.identity);
-            if (++j == 3)
-                j = 0;
-        }
-        if(gameObjects[0])
-        {
-            gameObjects[0].transform.Translate(-0.03f, 0, 0);
-            if (gameObjects[0].transform.position.x < -4)
-                Destroy(gameObjects[0]);
-        }
-        if (gameObjects[1])
-        {
-            gameObjects[1].transform.Translate(-0.03f, 0, 0);
-            if (gameObjects[1].transform.position.x < -4)
-                Destroy(gameObjects[1]);
-        }
-        if (gameObjects[2])
-        {
-            gameObjects[2].transform.Translate(-0.03f, 0, 0);
-            if (gameObjects[2].transform.position.x < -4)
-                Destroy(gameObjects[2]);
-        }
-        //부스터 생성
-        if (Time.time > boostTime)
-        {
-            boostTime = Time.time + 10.2f;
-            boosters[k] = (GameObject)Instantiate(Booster, new Vector3(4, Random.Range(-1f, 3.2f), 0), Quaternion.identity);
-            if (++k == 3)
-                k = 0;
-        }
-        if (boosters[0])
-        {
-            boosters[0].transform.Translate(-0.03f, 0, 0);
-            if (boosters[0].transform.position.x < -4)
-                Destroy(boosters[0]);
-        }
-        if (boosters[1])
-        {
-            boosters[1].transform.Translate(-0.03f, 0, 0);
-            if (boosters[1].transform.position.x < -4)
-                Destroy(boosters[1]);
-        }
-        if (boosters[2])
-        {
-            boosters[2].transform.Translate(-0.03f, 0, 0);
-            if (boosters[2].transform.position.x < -4)
-                Destroy(boosters[2]);
-        }
+        
+        
+        if (!isBoost)
+            MoveIdle();
+        else
+            MoveFast();
     }
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.gameObject.name == "Column(Clone)")
+        //부스터 먹음
+        if (col.gameObject.tag == "Booster")
+        {
+            Destroy(col.gameObject);
+            StartCoroutine(MakeHero());
+            isBoost = true;
+        }
+        //칼럼, 바닥 부딪힘
+        else if (col.gameObject.name == "Column(Clone)")
         {
             Score.text = (++i).ToString();
             BirdSound_2.Play();
@@ -118,12 +85,87 @@ public class OneCommand : MonoBehaviour
             BirdSound_4.Play();
             GameOver();
         }
-        if(col.gameObject.name == "Booster(Clone)")
+    }
+    //무적효과
+    IEnumerator MakeHero()
+    {
+        this.gameObject.layer = 9;
+        GetComponent<SpriteRenderer>().color = Color.red;
+        boostEffect.Play();
+        BoostSound.Play();
+        yield return new WaitForSeconds(5.0f);
+        for(int i = 0; i < 10; i++)
         {
-            rig.AddForce(Vector3.forward * 200);
+            GetComponent<SpriteRenderer>().color = Color.white;
+            yield return new WaitForSeconds(0.1f);
+            GetComponent<SpriteRenderer>().color = Color.red;
+            yield return new WaitForSeconds(0.1f);
+        }
+        this.gameObject.layer = 0;
+        GetComponent<SpriteRenderer>().color = Color.white;
+        boostEffect.Stop();
+        BoostSound.Stop();
+        MoveIdle();
+        isBoost = false;
+    }
+    void MoveFast()
+    {
+        //기둥 생성기
+        if (Time.time > NextTime)
+        {
+            NextTime = Time.time + 0.5f;
+            gameObjects[j] = (GameObject)Instantiate(Column, new Vector3(4, Random.Range(-1f, 3.0f), 0), Quaternion.identity);
+            if (++j == 3)
+                j = 0;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            if (gameObjects[i])
+            {
+                gameObjects[i].transform.Translate(-0.09f, 0, 0);
+                if (gameObjects[i].transform.position.x < -3)
+                    Destroy(gameObjects[i]);
+            }
         }
     }
+    void MoveIdle()
+    {
+        //기둥 생성기
+        if (Time.time > NextTime)
+        {
+            NextTime = Time.time + 2.0f;
+            gameObjects[j] = (GameObject)Instantiate(Column, new Vector3(4, Random.Range(-1f, 3.0f), 0), Quaternion.identity);
+            if (++j == 3)
+                j = 0;
+        }
 
+        //부스터 생성
+        if (Time.time > boostTime)
+        {
+            boostTime = Time.time + 10.0f;
+            boosters[k] = (GameObject)Instantiate(Booster, new Vector3(4, Random.Range(-1f, 3.2f), 0), Quaternion.identity);
+            if (++k == 3)
+                k = 0;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            if (gameObjects[i])
+            {
+                gameObjects[i].transform.Translate(-0.03f, 0, 0);
+                if (gameObjects[i].transform.position.x < -4)
+                    Destroy(gameObjects[i]);
+            }
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            if (boosters[i])
+            {
+                boosters[i].transform.Translate(-0.03f, 0, 0);
+                if (boosters[i].transform.position.x < -4)
+                    Destroy(boosters[i]);
+            }
+        }
+    }
     void GameOver()
     {
         //게임 오버
